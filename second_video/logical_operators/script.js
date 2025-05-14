e22aa5e7bfcd58df541b8dd138ab3459952345b4
Me-while-coding <prachurjoofficial@gmail.com>
2025-05-15 04:11:12 +0530

    added new files

---
+++ second_video/logical_operators/script.js
@@ -0,0 +1,12 @@
+// logical operators
+//logical And (&&)
+let a = 6 , b = 2;
+console.log(a>=b && a%b==0); // true + true = truw
+console.log(a<b && a%b==0); // false + true = false
+
+// logical OR (||)
+console.log(a<b || a%b==0); // false + true = true
+
+// logical NOT(!)
+
+console.log("!(6<5),",!(6<5));
