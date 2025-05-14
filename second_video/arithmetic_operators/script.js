312ef4dd5ae6bb699442f622408907ba26c44863
Me-while-coding <prachurjoofficial@gmail.com>
2025-05-14 10:01:10 +0530

    adding new files

---
+++ second_video/arithmetic_operators/script.js
@@ -0,0 +1,44 @@
+//this is a single line comment
+/*these are 
+multiline comments*/
+
+// let a = 5 , b= 2;
+// let c = a+b;
+// console.log("a =",a," &  b =",b)
+// console.log("a + b = ",c);
+// console.log("a - b = ",a-b);
+// console.log("a * b = ",a*b);
+// console.log("a / b = ",a/b);
+// console.log("a % b = ",a%b); //the modulus operator gives the remainder
+// console.log("a to the power of b = ",a**b)
+
+//Unary Operators
+
+let a = 5 , b= 2;
+a++; //6
+b--; //1
+console.log("a =",a," &  b =",b);
+
+//pre-increment , pre-decrement
+
+let c = 8;
+
+console.log("initial value of c = ",c);
+console.log("++c = ",++c);
+
+c=8;
+console.log("--c = ",--c);
+
+//post-increment , post-decrement
+
+let d = 6;
+console.log("initial value of d = ",d);
+
+console.log("d++ = ",d++);
+console.log(d);
+
+d = 6;
+
+console.log("d-- = ",d--);
+console.log(d);
+
---
+++ second_video/assignment_operators/script.js
@@ -0,0 +1,30 @@
+//Assignment Operators
+
+let a = 5;
+let b = 2;
+console.log("a = "+a+" & b = "+b);
+a += b; // a = a + b;
+console.log("a += b");
+console.log("a = ",a); // 7
+a=5;
+a -= b; // a = a - b;
+console.log("a -= b");
+console.log("a = ",a); // 3
+a=5;
+a /= b; // a = a / b;
+console.log("a /= b");
+console.log("a = ",a); // 2.5
+a=5;
+a %= b; // a = a % b;
+console.log("a %= b");
+console.log("a = ",a); // 1
+a=5;
+a *= b; // a = a * b;
+console.log("a *= b");
+console.log("a = ",a); // 10
+a=5;
+a **= b; // a = a ** b;
+console.log("a **= b");
+console.log("a = ",a); // 25
+
+
