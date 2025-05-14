e376153985a369de37caec6b91170e6d9207a2d1
Me-while-coding <prachurjoofficial@gmail.com>
2025-05-14 10:18:51 +0530

    added more files

---
+++ second_video/comparison_operators/script.js
@@ -0,0 +1,7 @@
+// Comparison Operators 
+
+console.log("5 == 3 , "+(5==3));
+console.log("5 != 3 , "+(5!=3));
+console.log("5 == '5' , "+(5 == '5')); //converts '5' to 5 and compares
+console.log("5 === '5' , "+(5 === '5')); //checks both datatype and value
+console.log("5 !== '5' , "+(5 !== '5'));
