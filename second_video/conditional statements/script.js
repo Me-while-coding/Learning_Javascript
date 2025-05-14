55af5823ffbddfe1d9da0e50854f4d020b073e54
Me-while-coding <prachurjoofficial@gmail.com>
2025-05-15 04:27:05 +0530

    new code added

---
+++ second_video/conditional statements/script.js
@@ -0,0 +1,21 @@
+let age = 14;
+
+if(age >= 18)
+{
+    console.log("you CAN vote");
+}
+
+else{
+    console.log("Sory, you CANNOT vote");
+}
+
+let num = 34456;
+let isEven = num%2==0;
+
+if(isEven == true){
+    console.log(num+" is even");
+}
+
+else{
+    console.log(num+" is odd");
+}
