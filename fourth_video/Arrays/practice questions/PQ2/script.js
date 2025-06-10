6b0c4604caeff0e6685be6aa8a9299a00e507871
Me-while-coding <prachurjoofficial@gmail.com>
2025-06-10 21:08:48 +0530

    tried a new concept

--- fourth_video/Arrays/practice questions/PQ2/script.js
+++ fourth_video/Arrays/practice questions/PQ2/script.js
@@ -2,11 +2,18 @@ let prices = [250,645,300,900,50];
 
 let newPrice; let idx = 0;
 
-for(let price of prices){
+// for(let price of prices){
+//     newPrice = price*0.9;
+//     prices[idx] = newPrice;
+//     idx++;
+// }
+
+// console.log(prices);
+
+prices.forEach(function calOffer(price,idx){
     newPrice = price*0.9;
     prices[idx] = newPrice;
-    idx++;
-}
+})
 
 console.log(prices);
 
